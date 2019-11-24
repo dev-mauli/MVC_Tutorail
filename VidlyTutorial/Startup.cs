0c51ae96a94ca972d5d9a7875a5ab32bc1ff2206
premlad <premlad961@gmail.com>
2019-11-24 13:16:16 +0530

    Add project files.

---
+++ VidlyTutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyTutorial.Startup))]
+namespace VidlyTutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
